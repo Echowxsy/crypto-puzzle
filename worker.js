c88d62a332efe2b99ab45c8b04b74d1abbfd4d39
Echowxsy <echowxsy@gmail.com>
2024-05-08 11:53:40 +0800

    feat: remove console.log

--- worker.js
+++ worker.js
@@ -1,13 +1,9 @@
 import Puzzle from "./src/index.ts";
 
 onmessage = async function(e) {
-  console.log('Worker: Message received from main script');
   let puzzleBuffer = Buffer.from(e.data, "base64");
   let puzzle = new Uint8Array(puzzleBuffer);
   let start = +Date.now();
-  console.log("start:", start);
   let solution = await Puzzle.solve(puzzle);
-  console.log('Worker result:',solution);
   postMessage(solution);
-  console.log("end:", +new Date() - start);
 }
